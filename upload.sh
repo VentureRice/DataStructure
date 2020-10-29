3df6b2ba5423955d14a02d14396f7b000fc06122
apple <apple@MacBook-Pro.local>
2020-10-29 13:41:11 +0800

    uf.md updated

--- upload.sh
+++ upload.sh
@@ -2,7 +2,7 @@
 git init
 git add .
 git commit -m 'uf.md updated'
-git remote rm origin
+#git remote rm origin
 git remote add origin https://github.com/VentureRice/DataStructure.git
 git push origin master
 
